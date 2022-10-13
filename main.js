168f7e85f8a5ba8c7eb4e394f50da3f1f91f11c5
JulianaTagami <jutagamiss@gmail.com>
2022-10-13 21:04:50 +0100

    remove Promisse

--- main.js
+++ main.js
@@ -13,9 +13,4 @@ async function buscaEndereco(cep) {
 
 }
 
-let ceps = ['001001000', '01001001'];
-let conjutoCeps = ceps.map(valores => buscaEndereco(valores));
-
-Promise.all(conjutoCeps).then(respostas => console.log(respostas));
-
 var cep = document.getElementById('cep');
